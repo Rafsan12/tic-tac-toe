cf1d98943d8871332de14fd3f922eef60d9b83c6
Rafsan12 <rafsan.web.2022@gmail.com>
2024-01-13 23:51:54 +0600

    use array function in handleClick and pass all square components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -16,22 +12,28 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquare = squares.slice();
+    nextSquare[i] = "X";
+    setSquares(nextSquare);
+  }
   return (
     <>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
