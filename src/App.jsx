c7cde61902f13ace48c98c538cbb1f2921880c29
Rafsan12 <rafsan.web.2022@gmail.com>
2024-01-13 23:01:27 +0600

    create a handleClick function and setValue

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-export default function Square() {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <>
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 landing-9 text-lg m-1">
-          X
-        </button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
